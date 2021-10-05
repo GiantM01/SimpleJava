0355bb0ed87a015f316d9e7a29c866830dde4a7d
GiantM01 <mritunjayupadhyay7@gmail.com>
2021-10-05 09:56:29 +0530

    Initial commit

---
+++ src/com/devon/SimpleCalc.java
@@ -0,0 +1,18 @@
+package com.devon;
+
+public class SimpleCalc {
+    public static void main(String[] args) {
+        int a=10,b=5;
+        int w,x,y,z;
+        w=a+b;
+        x=a-b;
+        y=a*b;
+        z=a/b;
+        System.out.println("Enter the value of a ="+a);
+        System.out.println("Enter the value of b ="+b);
+        System.out.println("Add ="+w);
+        System.out.println("Sub ="+x);
+        System.out.println("Mul ="+y);
+        System.out.println("Div ="+z);
+    }
+}
