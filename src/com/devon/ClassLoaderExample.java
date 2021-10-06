c54a31936cc8555c74314a61a5464cf3b9a1db6a
GiantM01 <73254630+GiantM01@users.noreply.github.com>
2021-10-06 10:54:42 +0530

    Rename src/com/devon/SimpleCalc.java to src/SimpleCalc.java

---
+++ src/com/devon/ClassLoaderExample.java
@@ -0,0 +1,9 @@
+package com.devon;
+
+public class ClassLoaderExample {
+    public static void main(String[] args){
+        Class c = ClassLoaderExample.class;
+        System.out.println(c.getClassLoader());
+        System.out.println(String.class.getClassLoader());
+    }
+}
