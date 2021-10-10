398017cb2bec17573653d5996de1c278b8e18cce
GiantM01 <73254630+GiantM01@users.noreply.github.com>
2021-10-10 11:17:55 +0530

    Add files via upload

---
+++ src/add.java
@@ -0,0 +1,10 @@
+package com.devon;
+
+public class add {
+    public static void main(String args[]){
+        int a,b,c;
+        a= b= 10;
+        c=a+b;
+        System.out.println(c);
+    }
+}
---
+++ src/addLowerType.java
@@ -0,0 +1,10 @@
+package com.devon;
+
+public class addLowerType {
+    public static void main(String[]args){
+        byte a= 10;
+        byte b= 10;
+        byte c = (byte)(a+b);
+        System.out.println(c);
+    }
+}
